d8dfab9ab78a49ce3d7afdc5b30869ac4700535d
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-19 18:43:46 -0400

    tipos de dados

--- Sintaxe-TiposDeDados/CriandoClasses/Program.cs
+++ Sintaxe-TiposDeDados/CriandoClasses/Program.cs
@@ -1,5 +1,18 @@
 ﻿using CriandoClasses.Models;
 
+string apresetacao = "Olá, seja bem-vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresetacao);
+Console.WriteLine("Valor da variável quantidade " + quantidade);
+Console.WriteLine("Valor da variável altura " + altura);
+Console.WriteLine("Valor da variável preco " + preco);
+
+
+
 Pessoa p = new Pessoa();
 
 p.Nome = "Testando classe pessoa";
