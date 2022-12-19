98da81bf1794bbc5b7b4b5727df0754d424ec241
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-19 19:57:19 -0400

    Operadores de atribuicões convesões

---
+++ Sintaxe-TiposDeDados/OperadoresDeAtribuicao/Operadores/Program.cs
@@ -0,0 +1,35 @@
+﻿// int a = 10;
+// int b = 20;
+// int c = a + b;
+
+//Operador de atribuição '='
+// c '=' recebe a + b
+
+// c += 5; //30 + 5
+
+
+//Convertendo tipos de dados;
+
+//Cast - Casting;
+
+// int a  = Convert.ToInt32("5");
+//int a = int.Parse("5");
+
+
+//Diferença entre parse e convert
+// o int.Parse() não aceita nulo, quebra o programa
+// o Convert.ToInt32() aceita nulo e retorna 0;
+
+// converter para string
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+//cast implicito
+
+int a = 5;
+double b = a;
+
+// c# obedece ordem de precendencia da matematica
+
+//int.TryParse() tenta converter o valor
