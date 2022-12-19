f4e212c083cc6220fd3eee463ae932e35354d96c
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-19 19:58:11 -0400

    If else e ifAninhados

---
+++ Sintaxe-TiposDeDados/OperadoresDeAtribuicao/OperadoresCondicionais/Program.cs
@@ -0,0 +1,21 @@
+﻿int quantidadeEmEstoque = 10;
+int quantidadeCompra = 3;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Qunatidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade em Compra: {quantidadeCompra}");
+Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida");
+}
+else if (quantidadeCompra <= quantidadeEmEstoque)
+{
+    Console.WriteLine("Venda realizada");
+}
+else
+{
+    Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque");
+}
+
