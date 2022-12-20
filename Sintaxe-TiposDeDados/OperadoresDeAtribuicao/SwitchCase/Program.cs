ce7669bb690eaa0f0ea89c211791a0c9b08ce480
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-19 20:09:28 -0400

    Switch case

--- Sintaxe-TiposDeDados/OperadoresDeAtribuicao/SwitchCase/Program.cs
+++ Sintaxe-TiposDeDados/OperadoresDeAtribuicao/SwitchCase/Program.cs
@@ -4,17 +4,9 @@ var letra = Console.ReadLine();
 switch (letra)
 {
     case "A" or "a":
-        Console.WriteLine("É uma vogal");
-        break;
     case "E" or "e":
-        Console.WriteLine("É uma vogal");
-        break;
     case "I" or "i":
-        Console.WriteLine("É uma vogal");
-        break;
     case "O" or "o":
-        Console.WriteLine("É uma vogal");
-        break;
     case "U" or "u":
         Console.WriteLine("É uma vogal");
         break;
