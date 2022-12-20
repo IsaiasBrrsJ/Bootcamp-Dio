2be775c18280cbaf43b5464657f7506dcd1678d1
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-19 21:30:58 -0400

    Operadores Aritmeticos

---
+++ OperadoresAritimeticos/ClasseCalculadora/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClasseCalculadora.Models;
+
+var calculadora = new Calculadora();
+
+// calculadora.Somar(1, 4);
+// calculadora.Multiplicar(2, 4);
+// calculadora.Dividir(9, 3);
+// calculadora.Subtrair(-2, 5);
+// calculadora.Potencia(3, 3);
+// calculadora.Seno(30);
+// calculadora.Tangente(30);
+// calculadora.Coseno(30);
+calculadora.RaizQuadrada(9);
+//Incremento
+
+int valor = 1;
+Console.WriteLine(valor);
+
+valor++;
+Console.WriteLine(valor);
+
+//Decrementear
+
+int valorII = 5;
+Console.WriteLine(valorII);
+valorII--;
+Console.WriteLine(valorII);
+
