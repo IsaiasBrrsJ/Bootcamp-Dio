c300df2885805e40a1640324be3d1ee99ee943f1
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-20 13:52:41 -0400

    Array e listas

---
+++ ArraysEhListas/Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 1;
+arrayInteiros[1] = 2;
+arrayInteiros[2] = 3;
+
+Console.WriteLine("Percorrendo array com FOR");
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    Console.WriteLine($"Posição Nº {i}: " + arrayInteiros[i]);
+}
+
+
+Console.WriteLine("Percorrendo array com FOREACH");
+foreach (int valor in arrayInteiros)
+{
+    Console.WriteLine(valor);
+}
+
+//Redimensionando um ARRAY
+
+// Array.Resize(ref arrayInteiros, arrayInteiros.Length + 1);
+
+//Copiando Array;
+
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
---
+++ ArraysEhListas/Listas/Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+
+foreach (string lista in listaString)
+{
+    Console.WriteLine(lista);
+}
