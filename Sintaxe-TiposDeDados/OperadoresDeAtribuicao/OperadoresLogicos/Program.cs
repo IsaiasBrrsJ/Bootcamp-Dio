520b04dabd5675a797847c9bcc2c51fcb66c5c73
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-19 20:49:30 -0400

    Operadores lógicos

---
+++ Sintaxe-TiposDeDados/OperadoresDeAtribuicao/OperadoresLogicos/Program.cs
@@ -0,0 +1,41 @@
+﻿bool ehMaiorDeIdade = true;
+bool possuiAutorizacaoDoResponsavel = false;
+
+//operador OR ||
+if (ehMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+{
+    Console.WriteLine("Entrada liberada");
+}
+else
+{
+    Console.WriteLine("Entrada não liberada");
+}
+
+//Operador AND &&
+
+bool possuiPresencaMinima = true;
+double media = 7.5;
+
+if (possuiPresencaMinima && media >= 7)
+{
+    Console.WriteLine("Aprovado");
+}
+else
+{
+    Console.WriteLine("Reprovado");
+}
+
+//Operador NOT (!) Negação de um valor
+
+bool choveu = true;
+bool estaTarde = true;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Pedalar");
+}
+else
+{
+    Console.WriteLine("Vou pedalar outro dia");
+}
+
