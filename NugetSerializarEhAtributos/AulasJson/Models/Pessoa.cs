2bfac2c840e90455f068b806352985d048721c99
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-12-21 15:04:56 -0400

    Aula serializando e deserilizando com Json

---
+++ NugetSerializarEhAtributos/AulasJson/Models/Pessoa.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NugetSerializarEhAtributos.Models
+{
+    /// <summary>
+    /// Classe que retornar nome e idade de um pessoa
+    /// </summary>
+    public class Pessoa
+    {
+
+        public Pessoa() { }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public void Deconstruct(out string nome, out string sobrenome)
+        {
+            nome = Nome;
+            sobrenome = Sobrenome;
+        }
+
+        private string _nome;
+        private int _idade;
+        public string Nome
+        {
+            get => _nome.ToUpper();
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+            }
+        }
+
+        public string Sobrenome { get; set; }
+        public string NomeCompleto { get => Nome + " " + Sobrenome; }
+
+
+        public int Idade
+        {
+            get => _idade;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Idade incorreta");
+                }
+
+                _idade = value
+                ++;
+            }
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {NomeCompleto.ToUpper()}, Idade: {Idade}");
+        }
+
+
+    }
+}
---
+++ NugetSerializarEhAtributos/AulasJson/Program.cs
@@ -0,0 +1,43 @@
+﻿using NugetSerializarEhAtributos.Models;
+using Newtonsoft.Json;
+
+Console.Clear();
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+
+List<VendaDeserializar> listaVenda = JsonConvert.DeserializeObject<List<VendaDeserializar>>(conteudoArquivo);
+
+foreach (VendaDeserializar venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToShortDateString()}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// List<Venda> listaVendas = new List<Venda>();
+// DateTime dataAtual = DateTime.Now;
+// //ISO 8601 padroniza a data entre sistemas;
+// //Praticando com Json;
+// Venda venda = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+// Venda venda2 = new Venda(2, "Licenças de software", 25.00M, dataAtual);
+
+// listaVendas.Add(venda);
+// listaVendas.Add(venda2);
+
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+
+// Console.WriteLine(serializado);
